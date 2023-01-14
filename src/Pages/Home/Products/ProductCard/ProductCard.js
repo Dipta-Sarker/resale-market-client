2c6d4968710c4eac960e80aa2e1a1fb7c0770e37
Dipta-Sarker <contact.diptasarker@gmail.com>
2023-01-14 13:58:01 +0600

    display laptop

---
+++ src/Pages/Home/Products/ProductCard/ProductCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({ product }) => {
+    const { img, name } = product
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <Link to='/home'>
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{name}</h2>
+            </div>
+            </Link>
+        </div>
+    )
+};
+
+export default ProductCard;
--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import ProductCard from './ProductCard/ProductCard';
 
 const Products = () => {
-    const {data} = useQuery({
+    const {data,isLoading} = useQuery({
         queryKey:['products'],
         queryFn: async () =>{
             const res = await fetch('products.json')
@@ -10,10 +11,19 @@ const Products = () => {
             return data
         }
 })
+
+if(isLoading){
+    return <progress className="progress progress-success w-56" value="100" max="100"></progress>
+}
 console.log(data)
     return (
         <div>
             <h1 className='text-4xl'>Products</h1>
+            <div className='grid gap-4 grid-cols-1 sm:grid-cols-2  md:grid-cols-3'>
+            {
+                data.map(product =><ProductCard product={product}></ProductCard>)
+            }
+            </div>
         </div>
     );
 };
