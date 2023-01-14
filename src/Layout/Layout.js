95dbb0ac4282e125c2cf9d02645e98419ee30e3b
Dipta-Sarker <contact.diptasarker@gmail.com>
2023-01-14 09:33:42 +0600

    footer added

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
 
 const Layout = () => {
@@ -7,6 +8,7 @@ const Layout = () => {
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
